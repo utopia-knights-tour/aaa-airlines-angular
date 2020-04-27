6e17e872a1e256afcb7d4aa02905cf5da9859d72
ace <allisterg@gmail.com>
2020-04-27 14:27:03 -0400

    payment data from flight choice working

--- src/app/_services/flight.service.ts
+++ src/app/_services/flight.service.ts
@@ -7,21 +7,12 @@ import { Flight } from '../_models/flight'
 })
 export class FlightService {
 
-  flight: Flight;
+ 
   constructor(private http: HttpClient) {}
 
   getFlightsForCounter(originCode:string, destinationCode:string, departureDate:string) {
     let query = `originCode=${originCode}&destinationCode=${destinationCode}&departureDate=${departureDate}`;
     return this.http.get(`https://meksvi4fnh.execute-api.us-east-1.amazonaws.com/dev/counter/flights?${query}`);
   }
-
-  storeFlight(flight : Flight) {
-    this.flight = flight;
-  }
-
-  retrieveFlight() {
-    return this.flight;
-  }
-
   
 }
