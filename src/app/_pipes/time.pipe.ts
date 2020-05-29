82a3020bf8c7d2c4c3ce4ccfca251f65538a82ce
ace <allisterg@gmail.com>
2020-05-29 14:06:06 -0400

    remove console.log

--- src/app/_pipes/time.pipe.ts
+++ src/app/_pipes/time.pipe.ts
@@ -9,7 +9,6 @@ export class TimePipe implements PipeTransform {
   constructor(private authService?: AuthService) { }
 
   transform(time: any): string {
-    console.log(time);
     if (this.authService.currentUserValue.role == 'agent' && typeof time != 'string') {
       return `${time['hour'] < 10 ? "0" : ""}${time['hour']}:0${time['minute']}:0${time['second']}`
     }
